451d96907bd896fd888c9a83c47cbf80adef38ab
b0nsky <ronnyboni.12@gmail.com>
2024-09-04 18:06:36 +0700

    fead:add guardAdmin feature

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const EventController = require("./controller/EventController");
 const authentication = require("./middlewares/authentication");
 const { errorHandler } = require("./middlewares/errorHandler");
+const { guardAdmin } = require("./middlewares/guardAdmin");
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,18 +19,18 @@ app.use(express.json())
 app.get('/pub/events', EventController.pubEvents)
 app.get('/pub/events/:id', EventController.onePubEvent)
 app.post('/login', EventController.login)
+app.post('/new-user', EventController.newUser)
 
 app.use(authentication)
 
 app.get("/events", EventController.getEvents)
-app.post("/events", EventController.addNewEvent)
+app.post("/events", guardAdmin, EventController.addNewEvent)
 app.get("/events/:id", EventController.oneEvent)
-app.put("/events/:id", EventController.updateEvent)
-app.delete("/events/:id", EventController.deleteEvent)
+app.put("/events/:id", guardAdmin, EventController.updateEvent)
+app.delete("/events/:id", guardAdmin, EventController.deleteEvent)
 app.get("/categories", EventController.getCategories)
-app.post("/categories", EventController.addNewCategory)
-app.put("/categories/:id", EventController.updateCategory)
-app.post("/new-user", EventController.newUser)
+app.post("/categories", guardAdmin, EventController.addNewCategory)
+app.put("/categories/:id", guardAdmin, EventController.updateCategory)
 
 app.use(errorHandler)
 
