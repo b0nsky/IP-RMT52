6df36095ebccbdba9f4d32c9f93978bd4c369fc5
b0nsky <ronnyboni.12@gmail.com>
2024-09-04 21:17:06 +0700

    feat: add html for pages

--- middlewares/guardAdmin.js
+++
@@ -1,9 +0,0 @@
-function guardAdmin(req, res, next) {
-    if(req.user.role !== "Admin") {
-        next({ name: "Forbidden", message: "You are not authorized"})
-        return
-    }
-    next()
-}
-
-module.exports = { guardAdmin }
