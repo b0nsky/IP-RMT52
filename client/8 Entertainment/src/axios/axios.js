9dc4ad9a3ee795ac733191d9a891a9fa5ec47815
b0nsky <ronnyboni.12@gmail.com>
2024-09-06 11:51:59 +0700

    fix: axios URL

--- client/8 Entertainment/src/axios/axios.js
+++ client/8 Entertainment/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:3000`
+    baseURL: `https://8entertainment.ronnybons.space`
 })
 
 export default axiosInstance
